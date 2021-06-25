5d56685fa1a6e79c6f786fe478ac2659536eb4d6
Yang <yayuan@tripadvisor.com>
2021-06-25 22:39:47 +1000

    add new expense form and learn about state

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,26 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import "./ExpenseItem.css";
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title);
+
+  const clickHander = () => {
+    setTitle('Updated');
+    console.log(title);
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>  
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHander}>change title</button>
     </Card>
   );
 }
---
+++ src/components/NewExpense/ExpenseForm.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+import "./ExpenseForm.css";
+
+const ExpenseForm = () => {
+  //const [enteredTitle, setEnteredTitle] = useState('');
+  //const [enteredAmound, setEnteredAmount] = useState('');
+  //const [enteredDate, setEnteredDate] = useState('');
+  const [userInput, setUserInput] = useState({
+    enteredTitle: '',
+    enteredAmount: '',
+    enteredDate: ''
+  });
+  
+  const titleChangeHandler = (event) => {
+    setUserInput({
+        ...userInput,
+        enteredTitle: event.target.value
+    });  
+  };
+
+  const amoundChangeHandler = event => {
+    setUserInput({
+        ...userInput,
+        enteredAmount: event.target.value
+    }); 
+  };
+
+  const dateChangeHandler = event => {
+    setUserInput({
+        ...userInput,
+        enteredDate: event.target.value
+    }); 
+  };
+
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>title</label>
+          <input type="text" onChange={titleChangeHandler}/>
+        </div>
+        <div className="new-expense__control">
+          <label>title</label>
+          <input type="number" min="0.01" setp="0.01" onChange={amoundChangeHandler}/>
+        </div>
+        <div className="new-expense__control">
+          <label>title</label>
+          <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler}/>
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+
+export default ExpenseForm;
